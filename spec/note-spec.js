eaf262af820ef9f8b842b8cb7a2c28489e7f5c5d
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-07 15:57:35 +0000

    1) Refactored the code to fit the module pattern. 2) Test passing!

--- spec/note-spec.js
+++ spec/note-spec.js
@@ -1,9 +1,9 @@
-var note = require('../src/note-model').Note;
-var isTrue = require('../lib/test-framework').isTrue;
+// var Note = require('../src/note-model');
+// var isTrue = require('../lib/test-framework').isTrue;
 
 function testNoteText (text) {
-  note = new Note(text);
-  isTrue(note.text === text);
+  var note = new Note(text);
+  assert.isTrue(note.text === text);
 }
 
 testNoteText("I love JavaScript");
