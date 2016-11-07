3bae8b73b632346aecb7febff9ce22c55aa515b5
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-07 17:43:14 +0000

    1) Modified the test framework to be more specific about what it is testing. 2) New test written to test new getText method. Passing.

--- spec/note-spec.js
+++ spec/note-spec.js
@@ -1,9 +1,13 @@
-// var Note = require('../src/note-model');
-// var isTrue = require('../lib/test-framework').isTrue;
-
 function testNoteText (text) {
   var note = new Note(text);
   assert.isTrue(note.text === text);
 }
 
 testNoteText("I love JavaScript");
+
+function testGetText (text) {
+  var note = new Note(text);
+  assert.isTrue(note.getText() === text);
+}
+
+testGetText("I really don't know what I'm doing");
