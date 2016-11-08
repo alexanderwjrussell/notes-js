481e7f4e4108af64f642eeb8e22d66f9c9de35a8
crispinandrews <crispin.andrews@gmail.com>
2016-11-08 16:11:04 +0000

    note controller added. Hello updated to howdy

---
+++ src/note-controller.js
@@ -0,0 +1,16 @@
+document.getElementById("app").innerHTML = "Howdy";
+
+// (function(exports) {
+//
+//   function NoteController (){
+//     this.noteList = new NoteList();
+//   }
+//
+// 	NoteController.prototype.changeText = function() {
+// 		appDiv = document.getElementById('app');
+//     appDiv.innerHTML = 'Howdy';
+//   };
+//
+//   exports.NoteController = NoteController;
+//   exports.changeText = this.changeText;
+// })(this);
